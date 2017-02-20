6a60be153a223335351d9a54b9f94d5751a92c4e
maxvanbel <maxvanbel@gmail.com>
2017-02-20 00:09:31 -0600

    got mongoose to save data from scraping

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ var app = express();
 var request = require('request');
 // Scrapes our HTML
 var cheerio = require('cheerio');
-var PORT= 3000;
+var PORT = 3000;
 mongoose.connect('mongodb://localhost/' + PORT);
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
@@ -21,9 +20,7 @@ var articlesSchema = new Schema({
     title: String,
     link: String
 });
-
 var Business = mongoose.model('Business', articlesSchema);
-
 // Make a request call to grab the HTML body from the site of your choice
 // First, tell the console what server.js is doing
 console.log("\n***********************************\n" + "Grabbing every thread name and link\n" + "from Washington Post's website:" + "\n***********************************\n");
@@ -33,8 +30,6 @@ app.get("/scrape", function (req, res) {
         // Load the HTML into cheerio and save it to a variable
         // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
         var $ = cheerio.load(html);
-        // An empty array to save the data that we'll scrape
-        var result = [];
         // With cheerio, find each p-tag with the "title" class
         // (i: iterator. element: the current element)
         $(".story-headline h3").each(function (i, element) {
@@ -43,28 +38,31 @@ app.get("/scrape", function (req, res) {
             // In the currently selected element, look at its child elements (i.e., its a-tags),
             // then save the values for any "href" attributes that the child elements may have
             var link = $(element).children().attr("href");
+            // console.log(result);
             // If this title element had both a title and a link
             if (title && link) {
                 // Save the data in the scrapedData db
-                db.businessArticles.save({
+                var listOfArticles = new Business({
                     title: title,
                     link: link
-                }, function (error, saved) {
-                    // If there's an error during this query
-                    if (error) {
-                        // Log the error
-                        console.log(error);
-                    }
-                    // Otherwise,
-                    else {
+                });
+                listOfArticles.save(function (err) {
+                    if (err) {
+                        return handleError(err);
+                        // saved!
+                    } else { // Otherwise,
                         // Log the saved data
-                        console.log(saved);
+                        console.log("title of article: " + listOfArticles.title);
+                        console.log("link of article: " + listOfArticles.link);
                     }
                 });
             }
+            // Log the result once cheerio analyzes each of its selected elements
         });
     });
+    res.send("Scrape Complete");
 });
+// This will send a "Scrape Complete" message to the browser
 // Listen on port 3000
 app.listen(3000, function () {
     console.log("App running on port 3000!");
